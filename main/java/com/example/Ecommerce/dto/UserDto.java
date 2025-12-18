0a27000a74915340140a23ae205bdf915008db1b
unknown <sudiksha085@gmail.com>
2025-12-18 22:44:17 +0545

    Added@NotEmpty @Pattern and @Size in UserDto

--- main/java/com/example/Ecommerce/dto/UserDto.java
+++ main/java/com/example/Ecommerce/dto/UserDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -17,10 +18,19 @@ public class UserDto {
    @NotEmpty(message="You must enter name")
    @Size(min=3,max=100,message="Your name must be between 3-100 characters")
     private String fullName;
-    private String email;
+    @Pattern(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",message = "Invalid Email !! please type a valid email.")
+   @NotEmpty(message = "Email cannot be empty")
+   private String email;
+    @NotEmpty(message = "Address field can not be Empty")
+    @Size(min = 5, max = 100, message = "Address field should be between 5-100 characters")
     private String address;
+    @NotEmpty(message = "Mobile number  can not be Empty")
+    @Pattern(regexp = "^\\+?(?:977)?[ -]?(?:(?:(?:98|97)-?\\d{8})|(?:01-?\\d{7}))$",message = "Invalid Mobile Number ,Please Enter Valid Number ")
+    @Size(min = 10,max = 10,message = "Mobile Number should be Exactly of 10 Digit")
     private String mobileNumber;
+    @NotEmpty(message = "UserName cannot be empty")
     private String userName;
+    @NotEmpty(message = "password cannot be empty")
     private String password;
     private LogonType logonType;
 
